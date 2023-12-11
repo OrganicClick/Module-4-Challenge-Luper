d74f638aa39512b166ebce38644f9843ec3abee5
Austin Luper <austin.luper@gmail.com>
2023-12-10 19:45:31 -0500

    Added quiz questions and their answers to const quizQuestions within the js file. Also added relevant comments to this section of code.

---
+++ script.js
@@ -0,0 +1,44 @@
+// Creates an array of quiz questions
+const quizQuestions = [
+    {
+        question: 'What is JavaScript?'
+        options: ['JavaScript is a scripting language used to make the website interactive', 
+        'JavaScript is an assembly language used to make the website interactive',
+         'JavaScript is a compiled language used to make the website interactive',
+         'None of the mentioned'],
+        correctAnswer: 'JavaScript is a scripting language used to make the website interactive'
+    },
+    {
+        question: 'Which of the following is correct about JavaScript?'
+        options: ['JavaScript is an Object-Based language',
+            'JavaScript is Assembly-language',
+            'JavaScript is an Object-Oriented language',
+            'JavaScript is a High-level language'],
+        correctAnswer: 'JavaScript is an Object-Based language'
+    }
+    {
+        question: 'Among the given statements, which statement defines closures in JavaScript?'
+        options: [ 'JavaScript is a function that is enclosed with references to its inner function scope',
+        'JavaScript is a function that is enclosed with references to its lexical environment',
+        'JavaScript is a function that is enclosed with the object to its inner function scope',
+        'None of the mentioned'],
+        correctAnswer: 'JavaScript is a function that is enclosed with references to its lexical environment'
+    }
+    {
+        question: 'Arrays in JavaScript are defined by which of the following statements?'
+        options: ['It is an ordered list of values',
+        'It is an ordered list of objects',
+        'It is an ordered list of string',
+        'It is an ordered list of functions'],
+        correctAnswer: 'It is an ordered list of values'
+    }
+    {
+        question: 'Which of the following is not javascript data types?'
+        options: ['Null type',
+        'Undefined type',
+        'Number type',
+        'All of the mentioned'],
+        correctAnswer: 'All of the mentioned'
+    }
+
+]
